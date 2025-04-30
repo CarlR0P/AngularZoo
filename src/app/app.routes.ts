8e7550769c33946fd9eaab6d81eb01e7a4dbbaa2
Carlos <sinNombre@gmail.com>
2025-04-29 20:28:17 -0500

    Primer Commit
    Proyecto Angular, con error

---
+++ src/app/app.routes.ts
@@ -0,0 +1,3 @@
+import { Routes } from '@angular/router';
+import { AnimalComponent } from './compos/animal/animal.component';
+export const routes: Routes = [{path: 'inicio', component: AnimalComponent}];
---
+++ src/app/services/animal.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnimalService {
+
+  apiUri = '/api/animals';
+  httpOptions = new HttpHeaders().set('Content-Type', 'application/json');
+  constructor(private http: HttpClient) { }
+
+  getAllAnimalsData(): Observable<any> {
+    return this.http.get<any>(this.apiUri)
+  }
+
+}
+
+
+
